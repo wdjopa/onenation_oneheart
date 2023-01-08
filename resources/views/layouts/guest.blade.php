d43e18a3f5fae7a1f3f623a3993a9306a3983815
Wilfried DJOPA <37931403+wdjopa@users.noreply.github.com>
2023-01-08 21:41:55 +0100

    adding google analytics

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,6 +15,15 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-SZLSCY3SQV"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-SZLSCY3SQV');
+        </script>
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
